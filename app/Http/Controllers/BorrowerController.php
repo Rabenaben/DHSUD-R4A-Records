fae38330ebfe2ac9b8ca16b613a16e07ab72502b
Xai <jenezyrelle0704@gmail.com>
2026-01-06 14:24:19 +0800

    Borrowed Dockt Count

--- app/Http/Controllers/BorrowerController.php
+++ app/Http/Controllers/BorrowerController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Borrower;
+use App\Models\RemDatabase;
+use App\Models\HoaDatabase;
 
 class BorrowerController extends Controller
 {
