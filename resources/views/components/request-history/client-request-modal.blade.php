4982babeeecf9d6265681370b4d15ea24fc721d5
Raven <ravenculis29@gmail.com>
2026-03-05 10:57:03 +0800

    removed hidden in client request modal to remove css conflict

--- resources/views/components/request-history/client-request-modal.blade.php
+++ resources/views/components/request-history/client-request-modal.blade.php
@@ -137,7 +137,7 @@
             </div>
 
             <!-- View Mode Buttons -->
-            <div class="hidden flex justify-end gap-3" id="view-mode-buttons">
+            <div class="flex justify-end gap-3" id="view-mode-buttons">
                 <button class="rounded-lg bg-gray-500 px-4 py-2 text-sm font-semibold text-white hover:bg-gray-600"
                     id="close-view-btn" type="button">
                     Close
